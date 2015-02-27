6c0be0dd1fa502421bbd5ecf06ecedb5e8425d0e
Jihoon Lee <jihoonlee.in@gmail.com>
2015-02-27 16:32:18 +0900

    Trimmed the branch to only the concert_service_manager sub directory

--- concert_utilities/src/concert_utilities/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-# License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_qt_gui/license/LICENSE
-#
-##############################################################################
-# Imports
-##############################################################################
-
-from . import conductor_graph
