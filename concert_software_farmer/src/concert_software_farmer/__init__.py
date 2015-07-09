114f489f70f95a860b677529e59a4f28cde5574d
Jihoon Lee <jihoonlee.in@gmail.com>
2015-07-09 14:59:49 +0900

    Trimmed the branch to only the concert_conductor sub directory

--- concert_software_farmer/src/concert_software_farmer/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .pool import SoftwarePool
-from .farmer import SoftwareFarmer
-from .client import SoftwareFarmClient, FailedToStartSoftwareException
-from .exceptions import InvalidSoftwareprofileException, SoftwareProfileException, SoftwareInstanceException, SoftwareNotExistException
-
