d7ef1d92cfd0ab7ad64fc174a1f2d7ea76f1b473
Jihoon Lee <jihoonlee.in@gmail.com>
2015-08-28 10:26:45 +0900

    Rebase from 'upstream'

--- concert_service_utilities/doc/make.bat
+++ concert_service_utilities/doc/make.bat
@@ -115,9 +115,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\concert_service_manager.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\concert_service_utilities.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\concert_service_manager.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\concert_service_utilities.ghc
 	goto end
 )
 
