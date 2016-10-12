822fa25246900cb529d5bed82b5bf0467da92ccc
Daniel Stonier <d.stonier@gmail.com>
2016-10-12 09:54:44 +0900

    Revert "Trimmed the branch to only the concert_service_manager sub directory"
    This reverts commit 31e3fbe5f69fc7dd93623b6818b9e4b7f5251aeb.

---
+++ concert_utilities/src/concert_utilities/__init__.py
@@ -0,0 +1,9 @@
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_qt_gui/license/LICENSE
+#
+##############################################################################
+# Imports
+##############################################################################
+
+from . import conductor_graph
